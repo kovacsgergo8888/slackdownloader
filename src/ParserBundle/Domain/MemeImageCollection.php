5c404ca5add54e99a827625a0062e41261435205
Gergely Kovács <kovacs.gergely8888@gmail.com>
2022-10-03 22:15:51 +0200

    #1 replace array creation
    try replace array creation with Slack objects

--- src/ParserBundle/Domain/MemeImageCollection.php
+++ src/ParserBundle/Domain/MemeImageCollection.php
@@ -7,6 +7,8 @@ use ArrayIterator;
 use Countable;
 use App\ParserBundle\Domain\Exception\DomainException;
 use App\ParserBundle\Domain\Slack\SlackPostCollection;
+use App\ParserBundle\Domain\Slack\File;
+use App\ParserBundle\Domain\Slack\SlackPost;
 use IteratorAggregate;
 use TypeError;
 
@@ -63,21 +65,23 @@ final class MemeImageCollection implements ArrayAccess, IteratorAggregate, Count
      */
     public static function createFromArray(array $data): self
     {
-        $urls = [];
+        $posts = [];
         foreach ($data as $slackPosts){
 
             if (!isset($slackPosts['files'])) {
                 throw new DomainException('Not correct Slack format!');
             }
 
+            $files = [];
             foreach ($slackPosts['files'] as $f){
                 if (isset($f['url_private_download'])){
-                    $urls[] = new MemeImage($f['url_private_download']);
+                    $files[] = new File($f['url_private_download']);
                 }
             }
+            $posts[] = new SlackPost(...$files);
         }
-
-        return new self(...$urls);
+        $postCollection = new SlackPostCollection(...$posts);
+        return self::createFromSlackPostCollection($postCollection);
     }
 
     public static function createFromSlackPostCollection(SlackPostCollection $slackPosts): self
